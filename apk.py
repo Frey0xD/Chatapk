04d917ad02f6c195d0fa2d3bcb85e250f942d36b
Frey <frey0xd@gmail.com>
2023-06-17 22:42:55 +0530

    add your chatgpt api key

--- apk.py
+++ apk.py
@@ -1,31 +1,23 @@
-import socket
+import openai
 
+openai.api_key = 'YOUR_API_KEY'
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def chat_with_gpt(prompt):
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        max_tokens=100,
+        n=1,
+        stop=None,
+        temperature=0.7
+    )
+    return response.choices[0].text.strip()
 
-
-s.bind(("127.0.0.1", 5000))
-
-sten for incoming connectionss.listen(5)
-
-
-conn, addr = s.accept()
-
-
-username = conn.recv(1024).decode("utf-8")
-
-
-conn.send("Welcome to the chat room, {}!".format(username).encode("utf-8"))
-
+print("ChatGPT: Hello! How can I assist you today?")
 
 while True:
-
-    message = conn.recv(1024).decode("utf-8")
-
-    
-    print(message)
-
-    conn.send("Your message was received!".encode("utf-8"))
-
-
-conn.close()
+    user_input = input("User: ")
+    if user_input.lower() == "bye":
+        break
+    response = chat_with_gpt("User: " + user_input + "\nChatGPT:")
+    print("ChatGPT:", response)
